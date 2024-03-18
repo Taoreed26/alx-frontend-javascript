4a155bfe57641f6a01cd91037e3185e89fd9ea08
Taoreed26 <117745831+Taoreed26@users.noreply.github.com>
2024-03-18 10:20:35 +0100

    main.ts

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,76 @@
+// Teacher's Interface
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience: number;
+  location: string
+  [propName: string]: any;
+}
+
+// Director's Interface
+interface Directors extends Teacher {
+  numberOfReports: number
+}
+
+// Creating Teacher, Director
+const teacher3: Teacher = {
+  firstName: 'John';
+  lastName: 'Doe';
+  location: 'Spain';
+  fullTimeEmployee: false;
+  contract: false;
+};
+console.log(teacher3);
+
+const director1: Directors= {
+  firstName: 'John';
+  lastName: 'Doe';
+  location: 'London';
+  fullTimeEmployee: true;
+  numbeOfReports: 17;
+};
+console.log(director1);
+
+// prints Teacher's name in this format: J. Doe
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+console.log(printTeacher('Harley', 'Searle'));
+
+// studentClass description interface
+interface studentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// constructor description interface
+interface classConstructor {
+  new (firstName: string, lastName: string): studentClassInterface;
+}
+// Creating class and constructor through interfaces
+class StudentClass implements classInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// creating instances of StudentClass
+const studentClass: StudentClass = new StudentClass('Nora', 'Sanchez');
+console.log(studentClass.displayName());
+console.log(studentClass.workOnHomework());
